44d99390266aa7ec8cde5020642d424a97c3dfcc
Lucas Bebber <lucasbbebber@gmail.com>
2019-10-02 19:29:34 -0300

    Update tests

--- react/__tests__/useCssHandles.test.tsx
+++ react/__tests__/useCssHandles.test.tsx
@@ -41,4 +41,34 @@ describe('useCssHandles', () => {
       element2: 'vtex-app-2-x-element2',
     })
   })
+  describe('migration', () => {
+    it('should add both the current app and the migration app', () => {
+      const CSS_HANDLES = ['element1', 'element2']
+
+      const handles = useCssHandles(CSS_HANDLES, {
+        migrationFrom: 'vtex.previous-app@3.0.0',
+      })
+
+      expect(handles).toStrictEqual({
+        element1:
+          'vtex-app-2-x-element1 vtex-app-2-x-element1--blockClass vtex-previous-app-3-x-element1 vtex-previous-app-3-x-element1--blockClass',
+        element2:
+          'vtex-app-2-x-element2 vtex-app-2-x-element2--blockClass vtex-previous-app-3-x-element2 vtex-previous-app-3-x-element2--blockClass',
+      })
+    })
+    it('should add more than one migration if needed', () => {
+      const CSS_HANDLES = ['element1', 'element2']
+
+      const handles = useCssHandles(CSS_HANDLES, {
+        migrationFrom: ['vtex.previous-app@2.0.0', 'vtex.previous-app@3.0.0'],
+      })
+
+      expect(handles).toStrictEqual({
+        element1:
+          'vtex-app-2-x-element1 vtex-app-2-x-element1--blockClass vtex-previous-app-2-x-element1 vtex-previous-app-2-x-element1--blockClass vtex-previous-app-3-x-element1 vtex-previous-app-3-x-element1--blockClass',
+        element2:
+          'vtex-app-2-x-element2 vtex-app-2-x-element2--blockClass vtex-previous-app-2-x-element2 vtex-previous-app-2-x-element2--blockClass vtex-previous-app-3-x-element2 vtex-previous-app-3-x-element2--blockClass',
+      })
+    })
+  })
 })
