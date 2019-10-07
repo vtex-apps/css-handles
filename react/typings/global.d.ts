f271c3eeae4b3ed8bd2385f7546e8bbe003e65e1
Joao Fidelis <joaogfidelis@gmail.com>
2019-10-07 12:08:48 -0300

    correctly type HOC and create global types

---
+++ react/typings/global.d.ts
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { TachyonsScaleInput } from '../hooks/tachyons'
+
+declare global {
+  interface CssHandlesOptions {
+    migrationFrom?: string | string[]
+  }
+  type CssHandlesInput = readonly string[]
+  type ValueOf<T extends readonly any[]> = T[number]
+  type CssHandles<T extends CssHandlesInput> = Record<ValueOf<T>, string>
+}
--- react/withCssHandles.tsx
+++ react/withCssHandles.tsx
@@ -2,15 +2,10 @@ import React from 'react'
 import useCssHandles from './useCssHandles'
 import { ComponentType } from 'react'
 
-interface CssHandlesOptions {
-  migrationFrom?: string | string[]
-}
-type CssHandlesInput = readonly string[]
-
 const withCssHandles = <T extends CssHandlesInput, ComponentProps>(
   handles: T,
   options?: CssHandlesOptions
-) => (Component: ComponentType<ComponentProps>) => {
+) => (Component: ComponentType<ComponentProps & { cssHandles: CssHandles<T> }>) => {
   const EnhancedComponent = (props: ComponentProps) => {
     const cssHandles = useCssHandles(handles, options)
 
